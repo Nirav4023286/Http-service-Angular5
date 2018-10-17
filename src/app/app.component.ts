09802987e2d7fe34575965c4a0db1d284632f22b
Nirav <nirajm2025@gmail.com>
2018-10-17 21:27:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http-angular5';
+  constructor(private http: Http) { }
+   httpdata;
+   // tslint:disable-next-line:use-life-cycle-interface
+   ngOnInit() {
+      this.http.get('http://jsonplaceholder.typicode.com/users').
+      map(
+         (response) => response.json()
+      ).
+      subscribe(
+         (data) => {this.displaydata(data); }
+      );
+   }
+   displaydata(data) {this.httpdata = data; }
 }
